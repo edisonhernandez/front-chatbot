e0783deda9db85f6d8f82165fd721e41ad397f3a
edisonhernandez <david-hernandezbgu@gmail.com>
2019-11-19 20:27:08 -0500

    front chat cobranzas

--- cobranzas/src/app/servicies/global.ts
+++
@@ -1,5 +0,0 @@
-export var GLOBAL = {
-    url:'http://localhost:6001/cobranzas/'
-}
-
-1719218016 
---
+++ src/app/servicies/global.ts
@@ -0,0 +1,5 @@
+export var GLOBAL = {
+    url:'http://192.168.10.221:6001/cobranzas/'
+}
+
+1725002206
